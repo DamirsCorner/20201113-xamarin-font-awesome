cbd9a02107b10d7b82598e930887c13c294ca124
Damir Arh <damir.arh@gmail.com>
2020-11-01 11:25:29 +0100

    Use embedded font as image source

--- FontAwesome/FontAwesome/AssemblyInfo.cs
+++ FontAwesome/FontAwesome/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesome5Regular.otf", Alias = "FontAwesome5Regular")]
